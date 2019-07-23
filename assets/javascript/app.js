f79d2523966131c6020e8792e07236f288fc91b9
Blue Bean <sl8305@hotmail.com>
2019-07-22 17:58:42 -0700

    created basic ending function to display the # questions correct and false

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,25 @@
 // create a function for the timer // starts on click action of the button
 
+
+
+function checkAnswer (){
+    // loop through and check the responses
+}
+
+
 // create ender functions
 //      ending notice - all done
 //      loops through and update the number of correct and incorrect answer
 
+function timeUp (){
+    let correctResponse = 0;
+    let incorrectResponse = 0;
+
+    // function to check the answer
+
+    // output the html
+    $('#main-text').text("Number of correct answers: " + correctResponse);
+    $('#main-text').append("Number of incorrect answers: " + incorrectResponse);
+}
+
+timeUp();
